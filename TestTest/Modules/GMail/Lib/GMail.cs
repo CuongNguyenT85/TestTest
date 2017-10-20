7cd359425d494ce04d72832b909854467b524789
Cuong Nguyen <CuongNguyenT85@gmail.com>
2017-10-20 16:36:03 -0700

    Initial commit

---
+++ TestTest/Modules/GMail/Lib/GMail.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using Automation.Core.Framework.Configuration;
+using Automation.Core.Framework.Controllers;
+using Automation.Core.Framework.Utilities;
+using Automation.Core.Framework.PageObjects.Webdriver;
+using Automation.Core.Framework.Parsers;
+
+
+namespace TestTest.GMail.Lib
+{
+    class GMail : AbstractPageCore
+    {
+        /// <summary>
+        /// Campaign Content Panel
+        /// Handles all actions for the Campagin Content Panel
+        /// </summary>
+        public GMail(WebdriverController webdriverController, [CallerFilePath] string callerFilePath = null) : base(webdriverController, callerFilePath)
+        {
+            // yml file setup
+            const string gmailPath = "TestTest.Modules.GMail.Lib.GMail.yml";
+            YamlParser = new YamlParser(gmailPath);
+
+            // Go to your iFrame
+            //GoToIframe("HomFrame");
+        }
+
+        public void SetLogin()
+        {
+            GetElement("UserName").SendKeys("VSe.CuongNguyent@gmail.com");
+        }
+
+        public void SetPassword()
+        {
+            GetElement("Password").SendKeys("Changemenow");
+        }
+
+        public void ClickNext()
+        {
+            ClickElement("NextButton") ;
+        }
+    }
+}
---
+++ TestTest/Modules/GMail/Test/TestCases.cs
@@ -0,0 +1,69 @@
+﻿using Automation.Core.Framework.Controllers;
+using NUnit.Framework;
+using Automation.Core.Framework.TestFixtures;
+using TestTest.GMail.Lib;
+
+namespace ApplitoolsTest
+{
+    [TestFixture]
+    public class TestCases : AbstractTestCore
+    {
+        WebdriverController wdc = new WebdriverController();
+
+        public TestCases()
+        {
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            ApplitoolsController.Instance.Init("TestTest.Modules.GMail.Lib.Eyes.yml");
+            ApplitoolsController.Instance.Open(wdc.ProtractorDriver, "appName", "testName2");
+            ApplitoolsController.Instance.SetBatchName("TestBatch");
+            ApplitoolsController.Instance.SetMatchLevel("Layout");
+        }
+        
+        [TearDown]
+        public void Teardown()
+        {
+            wdc.Dispose();
+            ApplitoolsController.Instance.Dispose();
+        }
+        
+        [Test]
+        public void TestApplitools()
+        {
+            
+            GMail gmail = new GMail(wdc);
+
+            ApplitoolsController.Instance.CheckWindow("Window1");
+
+            gmail.SetLogin();
+
+
+            ApplitoolsController.Instance.CheckWindow("Window2");
+
+            gmail.ClickNext();
+
+
+            ApplitoolsController.Instance.CheckWindow("Window3");
+
+            gmail.SetPassword();
+
+
+            ApplitoolsController.Instance.CheckWindow("Window4");
+
+
+            gmail.ClickNext();
+
+
+            ApplitoolsController.Instance.CheckWindow("Window5");
+
+            //ApplitoolsController.Instance.CheckElement("Element1", "Element1");
+            //ApplitoolsController.Instance.CheckElement("Element2", "Element2");
+            //ApplitoolsController.Instance.CheckRegionInFrame(":2p", "Region1", "Region1");
+
+
+        }
+    }
+}
